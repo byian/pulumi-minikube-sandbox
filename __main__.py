dd2e98af1525e692e607b082205817fe48003b63
Mikhail Buianov <buyanovmikhailv@gmail.com>
2024-11-21 12:33:30 +0100

    init

---
+++ __main__.py
@@ -0,0 +1,20 @@
+"""A Kubernetes Python Pulumi program"""
+
+import pulumi
+from pulumi_kubernetes.apps.v1 import Deployment
+
+app_labels = { "app": "nginx" }
+
+for i in range(10):
+    deployment = Deployment(
+        f'nginx-{i}',
+        spec={
+            "selector": { "match_labels": app_labels },
+            "replicas": i,
+            "template": {
+                "metadata": { "labels": app_labels },
+                "spec": { "containers": [{ "name": "nginx", "image": "nginx" }] }
+            },
+        })
+
+pulumi.export("name", deployment.metadata["name"])
